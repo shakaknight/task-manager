0ad3ff2d6238fc3801d9cbd8b9bc848a1534027b
shakaknight <ananddubey016@gmail.com>
2021-09-13 21:10:12 +0530

    Add new-list component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import {HttpClientModule} from '@angular/common/http';
+import { NewListComponent } from './pages/new-list/new-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TaskViewComponent
+    TaskViewComponent,
+    NewListComponent
   ],
   imports: [
     BrowserModule,
